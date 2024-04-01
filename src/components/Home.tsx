0b3f42dee904037396e24af2818472f4703953d8
Victor Prado <prado.victor1005@gmail.com>
2024-03-31 23:54:09 -0300

    fix: iniciando design da tela

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getCurrencysQuotes } from "../services/currencyService";
 import { useEffect, useState } from "react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import logo from '@/assets/logo.svg';
 
 interface currencyProps {
   USDBRL: {
@@ -48,7 +49,6 @@ interface currencyProps {
 }
 
 export function Home() {
-  // const dataLine = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }];
   const [selectedCurrency, setSelectedCurrency] = useState('BTC')
   const { data, isLoading, isError } = useQuery<currencyProps>({
     queryKey: ['currencyQuotes'],
@@ -56,10 +56,12 @@ export function Home() {
     refetchInterval: 5000,
   });
   const [currencyQuotes, setCurrencysQuotes] = useState<currencyProps[]>([]);
+  const [lastCurrencyQuote, setLastCurrencyQuote] = useState<currencyProps[]>([]);
 
   useEffect(() => {
     if (data) {
       setCurrencysQuotes(prevData => [...prevData, data]);
+      setLastCurrencyQuote([data]);
     }
   }, [data]);
 
@@ -76,51 +78,77 @@ export function Home() {
   if (isError) return <div>Erro ao carregar os dados</div>;
 
   return (
-    <div className=''>
-      <h1 className="border-2 border-sky-900 w-12">BTC</h1>
-      <Select defaultValue='BTC' onValueChange={(value) => setSelectedCurrency(value)} >
-        <SelectTrigger>
-          <SelectValue placeholder='Selecione a moeda' />
-        </SelectTrigger>
+    <div className='flex flex-col items-center justify-center'>
 
-        <SelectContent>
-          <SelectItem value="BTC">BTC</SelectItem>
-          <SelectItem value="USD">USD</SelectItem>
-          <SelectItem value="EUR">EUR</SelectItem>
-        </SelectContent>
-      </Select>
-      <div>
-        {selectedCurrency === 'BTC' ? (
-          <div>
-            <LineChart width={600} height={300} data={cotacoesBTC}>
-              <Line type="monotone" dataKey="ask" stroke="#8884d8" />
-              <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
-              <XAxis dataKey="create_date" allowDecimals={false} />
-              <YAxis dataKey="ask" />
-              <Tooltip />
-            </LineChart>
-          </div>
-        ) : selectedCurrency === 'USD' ? (
-          <div>
-            <LineChart width={600} height={300} data={cotacoesUSD}>
-              <Line type="monotone" dataKey="ask" stroke="#8884d8" />
-              <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
-              <XAxis dataKey="create_date" allowDecimals={false} />
-              <YAxis dataKey="ask" />
-              <Tooltip />
-            </LineChart>
-          </div>
-        ) : (
-          <div>
-            <LineChart width={600} height={300} data={cotacoesEUR}>
-              <Line type="monotone" dataKey="ask" stroke="#8884d8" />
-              <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
-              <XAxis dataKey="create_date" allowDecimals={false} />
-              <YAxis dataKey="ask" />
-              <Tooltip />
-            </LineChart>
-          </div>
-        )}
+      <div className='w-full flex items-center justify-evenly' >
+        <img src={logo} className='size-32' />
+        <span>Bem-Vindo à RZK Digital</span>
+      </div>
+
+      <div className='flex flex-col items-center gap-10'>
+        <div className='w-full flex items-center justify-center gap-8'>
+          <h1 className="text-4xl">{selectedCurrency}</h1>
+          {selectedCurrency === 'BTC' ? (
+            lastCurrencyQuote.map((quote) => (
+              <span className='text-3xl font-light text-green-light-rzk'>{quote.BTCBRL.ask}</span>
+            ))
+          ) : selectedCurrency === 'USD' ? (
+            lastCurrencyQuote.map((quote) => (
+              <span className='text-3xl font-light text-green-light-rzk'>{quote.USDBRL.ask}</span>
+            ))
+          ) : (
+            lastCurrencyQuote.map((quote) => (
+              <span className='text-3xl font-light text-green-light-rzk'>{quote.EURBRL.ask}</span>
+            ))
+          )}
+        </div>
+        <div className=''>
+          <Select defaultValue='BTC' onValueChange={(value) => setSelectedCurrency(value)} >
+            <SelectTrigger className='border-2 border-green-500 w-40 h-8' >
+              <SelectValue placeholder='Selecione a moeda' />
+            </SelectTrigger>
+
+            <SelectContent>
+              <SelectItem value="BTC">BTC</SelectItem>
+              <SelectItem value="USD">USD</SelectItem>
+              <SelectItem value="EUR">EUR</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+
+        <div>
+          {selectedCurrency === 'BTC' ? (
+            <div>
+              <LineChart width={600} height={300} data={cotacoesBTC}>
+                <Line type="monotone" dataKey="ask" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
+                <XAxis dataKey="create_date" allowDecimals={false} />
+                <YAxis dataKey="ask" />
+                <Tooltip />
+              </LineChart>
+            </div>
+          ) : selectedCurrency === 'USD' ? (
+            <div>
+              <LineChart width={600} height={300} data={cotacoesUSD}>
+                <Line type="monotone" dataKey="ask" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
+                <XAxis dataKey="create_date" allowDecimals={false} />
+                <YAxis dataKey="ask" />
+                <Tooltip />
+              </LineChart>
+            </div>
+          ) : (
+            <div>
+              <LineChart width={600} height={300} data={cotacoesEUR}>
+                <Line type="monotone" dataKey="ask" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
+                <XAxis dataKey="create_date" allowDecimals={false} />
+                <YAxis dataKey="ask" />
+                <Tooltip />
+              </LineChart>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'blue-rzk': '#091D42',
+      'green-rzk': '#5ABF9A',
+      'green-light-rzk': '#4DC1AE'
+    },
     extend: {},
   },
   plugins: [],
